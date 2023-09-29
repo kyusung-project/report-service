3909783181d4b750cf5f77caeb7a6da2c6c36d23
LeeKyuSung <ks5050577@gmail.com>
2023-09-29 18:14:31 +0900

    Update ServerResDto.java

--- src/main/java/kr/leekyusung/service/report/dto/ServerResDto.java
+++ src/main/java/kr/leekyusung/service/report/dto/ServerResDto.java
@@ -1,14 +1,12 @@
 package kr.leekyusung.service.report.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class ServerResDto {
     String serviceName;
     String url;
