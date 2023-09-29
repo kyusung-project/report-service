ed5f23d76c474408202e4c717fcf64f599463d2e
LeeKyuSung <ks5050577@gmail.com>
2023-09-29 18:07:09 +0900

    test

--- src/main/java/kr/leekyusung/service/report/schedule/ScheduledTasks.java
+++ src/main/java/kr/leekyusung/service/report/schedule/ScheduledTasks.java
@@ -14,7 +14,8 @@ import org.springframework.stereotype.Component;
 public class ScheduledTasks {
     private final ServerService serverService;
 
-    @Scheduled(fixedDelay = 1000 * 60 * 5) // 5분마다
+    //    @Scheduled(fixedDelay = 1000 * 60 * 5) // 5분마다
+    @Scheduled(fixedDelay = 1000 * 30)  // 30초마다 테스트
     public void runJob() {
         log.info("runJob");
         log.info(serverService.getServer().toString());
